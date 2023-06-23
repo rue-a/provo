8331f2b5684097e6d4c1b311d0d316c660ae3345
Arne Ruemmler <arne.ruemmler@gmail.com>
2023-06-23 08:59:19 +0200

    update setup

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
 
 setup(
     name="provo",
@@ -19,8 +18,6 @@ setup(
     author_email="arne.ruemmler@gmail.com",
     version="1.0.5",
     description="Construct  PROV-O compliant provenance graphs.",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
     url="https://github.com/rue-a/provo",
     packages=find_packages(".", exclude=["tests", "tests.*"]),
     install_requires=["rdflib", "validators", "uuid"],
